68ec6ebfec5c44f15d0f8ea9add2789f16bba923
nicosantux <nico.santuccio@gmail.com>
2021-09-08 21:15:40 -0300

    update state

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 
+import { useUser } from '../hooks/useUser'
 import { useRandomAvatar } from '../hooks/useRandomAvatar'
 import Spinner from '../components/Spinner'
 import { Main, Avatar, Btn } from '../elements/homePage'
 
 const HomePage = () => {
+  const { setUserInfo } = useUser()
   const { avatarURL, isLoading, error } = useRandomAvatar()
   const history = useHistory()
+
+  useEffect(() => {
+    setUserInfo({
+      gender: '',
+      name: '',
+      email: '',
+      birthDate: new Date(),
+    })
+  }, [setUserInfo])
 
   const goToForm = () => {
     history.push('/form')
